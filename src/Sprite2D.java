55e763a206f773b098fa123a2f257a607e6d1c80
Grayson S Charlier <36171319+grayigami@users.noreply.github.com>
2018-10-19 14:54:10 -0500

    Update Sprite2D.java

--- src/Sprite2D.java
+++ src/Sprite2D.java
@@ -1,9 +1,36 @@
 public class Sprite2D {
 
 	private int spriteID;
+	private int sprPosX;
+	private int sprPosY;
+	private int sprWidth;
+	private int sprHeight;
+	private int[][] sprite
 	
-	public Sprite2D(int id) {
+	public Sprite2D() {
 		
-		spriteID = id;
+		spriteID = 0;
+		sprPosX = 0;
+		sprPosY = 0;
+		sprWidth = 1;
+		sprHeight = 1;
+		sprite = new int[sprWidth][sprHeight];
+		
 	}
+	
+	public int getPosX() 
+		return sprPosX;
+	
+	public int getPosY() 
+		return sprPosY;
+	
+	public int getHeight() 
+		return sprHeight;
+	
+	public int getWidth() 
+		return sprWidth;
+	
+	public int getPixel(int x, int y)
+		return sprite[x][y];
+	
 }
