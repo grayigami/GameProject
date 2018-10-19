66df31a0902ec5816a61f57cf68181470263f525
Grayson S Charlier <36171319+grayigami@users.noreply.github.com>
2018-10-19 14:34:03 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,13 @@
 public class Game {
 
 	public static boolean isRunning;
-	public static boolean isPaused;
+	public static boolean isPlaying;
 
 	public static void main(String[] args) {
 	
+		
 		while (isRunning) {
-			Update();
 			
-			Render();
-		}
-	}
-	
-	private static void Update() {
-	
-	}
-	
-	public static void Render() {
-	
-	}
+		}	
+	}	
 }
